54ff0a99fdf738b6d342c2af22de5663840d41a0
Antonia Goaga <goagaantonia@gmail.com>
2023-06-03 01:07:51 +0300

    users can close their connection

--- client.py
+++ client.py
@@ -1,70 +1,78 @@
 import threading, selectors, socket, rsa, sys, random, os, glob, time
 from colorama import Fore
 
-number_start = random.randint(1, 1000)
-number_count = random.randint(1, 20)
-numbers = []
-while len(numbers) < number_count:
-    numbers.append(number_start)
-    number_start += 1
-(cpubKey, cprivKey) = rsa.newkeys(1024)
-for i in numbers:
-    with open(
-        f"C:/Users/antonia/Desktop/Project/client_keys/pubKey{i}.pem", "wb+"
-    ) as f:
-        f.write(cpubKey.save_pkcs1("PEM"))
-    with open(
-        f"C:/Users/antonia/Desktop/Project/client_keys/privKey{i}.pem", "wb+"
-    ) as f:
-        f.write(cprivKey.save_pkcs1("PEM"))
-choice = random.choice(numbers)
-with open(
-    f"C:/Users/antonia/Desktop/Project/client_keys/pubKey{choice}.pem", "rb"
-) as f:
-    pubKey = rsa.PublicKey.load_pkcs1(f.read())
-with open(
-    f"C:/Users/antonia/Desktop/Project/client_keys/privKey{choice}.pem", "rb"
-) as f:
-    privKey = rsa.PrivateKey.load_pkcs1(f.read())
-for filename in glob.glob(f"C:/Users/antonia/Desktop/Project/client_keys/*.pem"):
-    os.remove(filename)
+buffer_size = 1024
 
 
 class ChatClient:
     def __init__(self, host, port):
-        self._host = host
-        self._port = port
-        self._username = username
-        self._pubkey, self._privkey = pubKey, privKey
-        self._serverkey = None
-        self._threadsend = threading.Thread(target=self.input)
-        self._threadrecv = threading.Thread(target=self.recieve)
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._read_selector = selectors.DefaultSelector()
+        self.host = host
+        self.port = port
+        self.username = username
+        self.pubkey, self.privkey = None, None
+        self.serverkey = None
+        self.threadsend = threading.Thread(target=self.input)
+        self.threadrecv = threading.Thread(target=self.recieve)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.read_selector = selectors.DefaultSelector()
+
+    def generate(self):
+        number_start = random.randint(1, 1000)
+        number_count = random.randint(1, 20)
+        numbers = []
+        while len(numbers) < number_count:
+            numbers.append(number_start)
+            number_start += 1
+        (cpubKey, cprivKey) = rsa.newkeys(buffer_size)
+        for i in numbers:
+            with open(
+                f"C:/Users/antonia/Desktop/Project/client_keys/pubKey{i}.pem", "wb+"
+            ) as f:
+                f.write(cpubKey.save_pkcs1("PEM"))
+            with open(
+                f"C:/Users/antonia/Desktop/Project/client_keys/privKey{i}.pem", "wb+"
+            ) as f:
+                f.write(cprivKey.save_pkcs1("PEM"))
+        choice = random.choice(numbers)
+        with open(
+            f"C:/Users/antonia/Desktop/Project/client_keys/pubKey{choice}.pem", "rb"
+        ) as f:
+            self.pubkey = rsa.PublicKey.load_pkcs1(f.read())
+        with open(
+            f"C:/Users/antonia/Desktop/Project/client_keys/privKey{choice}.pem", "rb"
+        ) as f:
+            self.privkey = rsa.PrivateKey.load_pkcs1(f.read())
+        for filename in glob.glob(
+            f"C:/Users/antonia/Desktop/Project/client_keys/*.pem"
+        ):
+            os.remove(filename)
 
     def input(self):
         while True:
             msg = input()
             final = username + ":" + msg
             try:
-                self._socket.send((rsa.encrypt(final.encode("ascii"), self._serverkey)))
+                self.socket.send((rsa.encrypt(final.encode("ascii"), self.serverkey)))
+                if msg == "quit":
+                    self.socket.close()
+                    break
             except:
                 break
 
     def connect(self):
-        self._socket.connect((self._host, self._port))
-        self._stringkey = self._socket.recv(1024)
-        self._serverkey = rsa.PublicKey.load_pkcs1(self._stringkey, format="DER")
-        s = self._socket.recv(1024).decode("ascii")
+        self.socket.connect((self.host, self.port))
+        self.stringkey = self.socket.recv(buffer_size)
+        self.serverkey = rsa.PublicKey.load_pkcs1(self.stringkey, format="DER")
+        s = self.socket.recv(buffer_size).decode("ascii")
         if s == "KEY":
-            self._socket.send(self._pubkey.save_pkcs1(format="DER"))
-        self._threadsend.start()
-        self._threadrecv.start()
+            self.socket.send(self.pubkey.save_pkcs1(format="DER"))
+        self.threadsend.start()
+        self.threadrecv.start()
 
     def recieve(self):
         while True:
             try:
-                msg = rsa.decrypt(self._socket.recv(1024), self._privkey).decode(
+                msg = rsa.decrypt(self.socket.recv(buffer_size), self.privkey).decode(
                     "ascii"
                 )
                 print(
@@ -75,14 +83,18 @@ class ChatClient:
                     + msg.split(":", 1)[1]
                     + Fore.RESET
                 )
+            except ConnectionAbortedError:
+                print("you closed the connection!")
+                self.socket.close()
+                break
             except:
                 print("an error occured!")
-                self._socket.close()
+                self.socket.close()
                 break
 
 
 if __name__ == "__main__":
     username = input("choose your username sir: ")
     client = ChatClient("localhost", 7342)
+    client.generate()
     client.connect()
-    sys.exit(1)
--- server.py
+++ server.py
@@ -2,50 +2,61 @@ import selectors, socket, rsa, threading
 
 
 clients = {}
+buffer_size = 1024
 
 
 class ChatServer:
     def __init__(self, host, port):
         """Initialise the server attributes."""
-        self._host = host
-        self._port = port
-        self._th = threading.Thread(target=self.receive)
-        self._username = None
-        self._socket = None
-        self._pubKey, self._privKey = None, None
-        self._clientkey = None
-        self._read_selector = selectors.DefaultSelector()
-        self._write_selector = selectors.DefaultSelector()
+        self.host = host
+        self.port = port
+        self.th = threading.Thread(target=self.receive)
+        self.username = None
+        self.socket = None
+        self.pubKey, self.privKey = None, None
+        self.clientkey = None
+        self.read_selector = selectors.DefaultSelector()
+        self.write_selector = selectors.DefaultSelector()
 
     def accept(self, sock):
         """Callback function for when the server is ready to accept a connection."""
         client, _ = sock.accept()
         print("Registering client...")
-        client.send(self._pubKey.save_pkcs1(format="DER"))
+        client.send(self.pubKey.save_pkcs1(format="DER"))
         client.send("KEY".encode("ascii"))
-        stringkey = client.recv(1024)
-        self._clientkey = rsa.PublicKey.load_pkcs1(stringkey, format="DER")
-        clients[client] = self._clientkey
-        self._read_selector.register(client, selectors.EVENT_READ, self.receive)
-        self._write_selector.register(client, selectors.EVENT_WRITE)
+        stringkey = client.recv(buffer_size)
+        self.clientkey = rsa.PublicKey.load_pkcs1(stringkey, format="DER")
+        clients[client] = self.clientkey
+        self.read_selector.register(client, selectors.EVENT_READ, self.receive)
+        self.write_selector.register(client, selectors.EVENT_WRITE)
 
     def receive(self, sock):
         """Callback function for when a client socket is ready to receive."""
-        msg = rsa.decrypt(sock.recv(1024), self._privKey).decode("ascii")
-        print(msg.split(":", 1)[1])
-        for key, _ in self._write_selector.select(0):
-            if key.fileobj is not sock:
-                key.fileobj.send(rsa.encrypt(msg.encode("ascii"), clients[key.fileobj]))
+        try:
+            msg = rsa.decrypt(sock.recv(buffer_size), self.privKey).decode("ascii")
+            print(msg.split(":", 1)[1])
+            if msg.split(":", 1)[1] != "quit":
+                for key, _ in self.write_selector.select(0):
+                    if key.fileobj is not sock:
+                        key.fileobj.send(
+                            rsa.encrypt(msg.encode("ascii"), clients[key.fileobj])
+                        )
+        except ConnectionResetError:
+            print("user closed the connection")
+            self.read_selector.unregister(sock)
+            self.write_selector.unregister(sock)
+            del clients[sock]
+            sock.close()
 
     def init(self):
         """Initialises the server socket."""
 
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.bind((self._host, self._port))
-        self._socket.listen()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen()
         # Put the socket in the selector "bag":
-        self._read_selector.register(
-            self._socket,
+        self.read_selector.register(
+            self.socket,
             selectors.EVENT_READ,
             self.accept,
         )
@@ -58,13 +69,13 @@ class ChatServer:
         with open(
             f"C:/Users/antonia/Desktop/Project/server_keys/pubKey.pem", "rb"
         ) as f:
-            self._pubKey = rsa.PublicKey.load_pkcs1(f.read())
+            self.pubKey = rsa.PublicKey.load_pkcs1(f.read())
         with open(
             f"C:/Users/antonia/Desktop/Project/server_keys/privKey.pem", "rb"
         ) as f:
-            self._privKey = rsa.PrivateKey.load_pkcs1(f.read())
+            self.privKey = rsa.PrivateKey.load_pkcs1(f.read())
         while True:
-            for key, _ in self._read_selector.select():
+            for key, _ in self.read_selector.select():
                 sock, callback = key.fileobj, key.data
                 callback(sock)
 
